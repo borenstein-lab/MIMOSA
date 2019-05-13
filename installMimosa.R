add8952de585ecbebc5b70eb8b182d243c6bd823
cnoecker <cecilia.noecker@gmail.com>
2019-05-13 13:54:16 +0300

    installation script

---
+++ installMimosa.R
@@ -0,0 +1,18 @@
+## Install dependencies from CRAN and Bioconductor
+
+cran_packages = c("devtools", "data.table", "Rcpp", "BiocManager")
+for (package in cran_packages){
+ if(!requireNamespace(package, quietly = T)){
+   install.packages(package)
+ }
+}
+
+bioc_packages = c("qvalue", "KEGGREST")
+for (package in bioc_packages){
+ if(!requireNamespace(package, quietly = T)){
+  BiocManager::install(package)
+ }
+}
+
+## Install MIMOSA
+install.packages("mimosa/", repos=NULL, type = "source")
